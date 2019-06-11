6e4676b7032f40a125d2109c6043275467c87170
Rickecr <richelton14@gmail.com>
2019-06-11 19:38:00 -0300

    first commit

---
+++ src/main/java/com/cursosufcg/cursosufcg/CursosufcgApplication.java
@@ -0,0 +1,13 @@
+package com.cursosufcg.cursosufcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosufcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosufcgApplication.class, args);
+	}
+
+}
