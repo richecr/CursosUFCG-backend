9c4fe8b9af337e53183bb99da64295dfd35215b7
Rich Elton <richelton14@gmail.com>
2019-06-12 17:25:43 -0300

    Entidade usuário criado

---
+++ src/main/java/com/cursosufcg/models/Usuario.java
@@ -0,0 +1,54 @@
+package com.cursosufcg.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	private String email;
+	private String primeiroNome;
+	private String ultimoNome;
+	private String senha;
+	
+	public Usuario(String email, String primeiroNome, String ultimoNome, String senha) {
+		super();
+		this.email = email;
+		this.primeiroNome = primeiroNome;
+		this.ultimoNome = ultimoNome;
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getUltimoNome() {
+		return ultimoNome;
+	}
+
+	public void setUltimoNome(String ultimoNome) {
+		this.ultimoNome = ultimoNome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
