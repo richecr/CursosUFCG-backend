4c3f95e4b0359be9f15861178facc32610c4bb38
Rickecr <richelton14@gmail.com>
2019-06-15 16:10:12 -0300

    Adicionando permissão de cors

--- src/main/java/com/cursosufcg/CursosufcgApplication.java
+++ src/main/java/com/cursosufcg/CursosufcgApplication.java
@@ -7,9 +7,24 @@ import org.springframework.context.annotation.Bean;
 
 import com.cursosufcg.authentication.TokenFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @SpringBootApplication
 public class CursosufcgApplication {
 
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
+	
 	@Bean
 	public FilterRegistrationBean<TokenFilter> filterJwt() {
 		try {
